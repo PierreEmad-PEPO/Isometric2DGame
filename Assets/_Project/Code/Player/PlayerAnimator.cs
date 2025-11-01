fa10734d085f1e14fc53ac884d850ee961ac9284
Pierre Emad <piereemorgan@gmail.com>
2025-11-02 00:56:52 +0200

    Made Environment Depth, Added some extras

---
+++ Assets/_Project/Code/Player/PlayerAnimator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator.GetComponentInChildren<Animator>();
+    }
+
+
+}
